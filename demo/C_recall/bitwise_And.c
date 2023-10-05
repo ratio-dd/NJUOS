b2991a6ff736dc8628c5327b7743412aa1a7e30c
ratio <ratio_dd@163.com>
2023-10-05 00:24:25 +0000

    Recall C synatx with some demo

---
+++ demo/C_recall/bitwise_And.c
@@ -0,0 +1,24 @@
+/* itoa example */
+#include <stdio.h>
+
+char *itoa(long i, char* s, int dummy_radix) {
+    sprintf(s, "%ld", i);
+    return s;
+}
+
+//test
+
+int main ()
+{
+  int i;
+  char buffer [33];
+  printf ("Enter a number: ");
+  scanf ("%d",&i);
+  itoa (i,buffer,10);
+  printf ("decimal: %s\n",buffer);
+  itoa (i,buffer,16);
+  printf ("hexadecimal: %s\n",buffer);
+  itoa (i,buffer,2);
+  printf ("binary: %s\n",buffer);
+  return 0;
+}
