505f29ad65cd9136ee5b04a1bc284f1836ab9d73
ratio <ratio_dd@163.com>
2023-10-07 09:31:23 +0000

    Add test printf gdb debug c file

---
+++ demo/C_recall/printftest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    /*gdb can't find write().
+    It shows in io directory, but glibc source code don't have that. */
+    write(1,"Hello world!\n",12);
+    printf("%d + %d = %d",1,1,1+1);
+}
