42465f3a47d0fe9d18551a6a8bba03237fc72e45
ratio <ratio_dd@163.com>
2023-10-13 08:08:06 +0000

    change calculator.c

--- demo/C_recall/Calculator.c
+++ demo/C_recall/Calculator.c
@@ -5,55 +5,64 @@
 #define NUMBER '0'
 #define MAXVAL 100
 
-int main(){
-    int type;
-    double op2;
-    char s[MAXOP];
-    if((type = getop(s))!=EOF){
-        switch (type)
-        {
-        case NUMBER:
-            push(atof(s));
-            break;
-        case '+':
-            push(pop()+pop());
-            break;
-        case '-':
-            op2 = pop();
-            push(pop()-op2);
-            break;
-        case '*':
-            push(pop()*pop());
-            break;
-        case '/':
-            op2 = pop();
-            if (op2!=0.0)
-            {
-                push(pop()/op2);
-            }else{
-                printf("error: devisor can't be 0");
-            }
-            break;
-        case '\n':
-            printf("\t%.8g\n",pop());
-            break;
-        
-        default:
-            printf("error: Unknow command");
-            break;
-        }
-    }
-}
-
 int sp = 0;
 double val[MAXVAL];
 
-double pop(void){
+void push(double f) {
+    if (sp < MAXVAL) {
+        val[sp++] = f;
+    } else {
+        printf("error: stack full!");
+    }
+}
+
+double pop() {
+    if (sp > 0) {
+        return val[--sp];
+    } else {
+        printf("error: stack empty!");
+        return 0.0;
+    }
+}
+
+double getop(char[]) {
 
 }
-void push(double){
 
-}
-double getop(char[]){
+int main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+    if ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0) {
+                    push(pop() / op2);
+                } else {
+                    printf("error: devisor can't be 0");
+                }
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
 
+            default:
+                printf("error: Unknow command");
+                break;
+        }
+    }
 }
